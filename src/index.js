9b017446f45f588b519a8994ca1bd00116d6f6cc
Minhchau0108 <minhchau3315@gmail.com>
2022-07-11 16:34:36 +0700

    add function has Vowels

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ String.prototype.isQuestion = function isQuestion() {
   const questionable = new RegExp(/\?$/);
   return questionable.test(this);
 };
+String.prototype.hasVowels = function hasVowels() {
+  const vowels = new RegExp("[aeiou]", "i");
+  return vowels.test(this);
+};
