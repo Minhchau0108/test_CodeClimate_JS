2f6d9c775a9f26f26dcc1d5e920b9589896e771d
Minhchau0108 <minhchau3315@gmail.com>
2022-07-12 08:57:13 +0700

    add function toUpper

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ String.prototype.hasVowels = function hasVowels() {
   const vowels = new RegExp("[aeiou]", "i");
   return vowels.test(this);
 };
+String.prototype.toUpper = function toUpper() {
+  const upper = new RegExp("[a-z]", "g");
+  return this.replace(upper, function transform(letter) {
+    return String.fromCharCode(letter.charCodeAt(0) - 32);
+  });
+};
